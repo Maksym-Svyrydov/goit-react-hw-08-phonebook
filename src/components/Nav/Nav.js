4c085c30144793dcdea58f618202e61d8dd4bcdc
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-04-14 16:56:17 +0300

    refactored authslise

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,17 +1,20 @@
 import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import css from './Nav.module.css';
-import Button from '@mui/material/Button';
+
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <nav>
-      <Button variant="outlined">
-        <NavLink to="/">Home</NavLink>
-      </Button>
-      {isLoggedIn && <NavLink to="/contacts">PhoneBook</NavLink>}
+      <NavLink className={css.link} to="/">
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={css.link} to="/contacts">
+          PhoneBook
+        </NavLink>
+      )}
     </nav>
   );
 };
-// className={css.link}
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -11,34 +11,82 @@ const initialState = {
 const authSlice = createSlice({
   name: 'auth',
   initialState: initialState,
-  extraReducers: {
-    [register.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logIn.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
-    [logOut.fulfilled](state) {
-      state.user = { name: null, email: null };
-      state.token = null;
-      state.isLoggedIn = false;
-    },
-    [refreshUser.pending](state) {
-      state.isRefreshing = true;
-    },
-    [refreshUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-    },
-    [refreshUser.rejected](state) {
-      state.isRefreshing = false;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(register.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(register.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      })
+      .addCase(logIn.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(logIn.fulfilled, (state, { payload }) => {
+        state.user = payload.user;
+        state.token = payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      })
+      .addCase(logOut.pending, state => {
+        state.isLoading = true;
+      })
+      .addCase(logOut.fulfilled, () => {
+        return initialState;
+      })
+      .addCase(logOut.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.error = payload;
+      })
+      .addCase(refreshUser.pending, state => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, { payload }) => {
+        state.user = payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, state => {
+        state.isRefreshing = false;
+      });
   },
+  // extraReducers: {
+  //   [register.fulfilled](state, action) {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [logIn.fulfilled](state, action) {
+  //     state.user = action.payload.user;
+  //     state.token = action.payload.token;
+  //     state.isLoggedIn = true;
+  //   },
+  //   [logOut.fulfilled](state) {
+  //     state.user = { name: null, email: null };
+  //     state.token = null;
+  //     state.isLoggedIn = false;
+  //   },
+  //   [refreshUser.pending](state) {
+  //     state.isRefreshing = true;
+  //   },
+  //   [refreshUser.fulfilled](state, action) {
+  //     state.user = action.payload;
+  //     state.isLoggedIn = true;
+  //     state.isRefreshing = false;
+  //   },
+  //   [refreshUser.rejected](state) {
+  //     state.isRefreshing = false;
+  //   },
+  // },
 });
 
 export const authReducer = authSlice.reducer;
